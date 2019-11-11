08912e91beae9d9347bcaaad0ec3f55c2bd1707f
zxsteven <breathesteve@gmail.com>
2019-11-10 21:48:52 -0600

    updated

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,8 @@ const Search = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
       <input 
-      className='pa3 ba b--green bg-lightest-blue'
+        aria-label='Search RoboDawgs'
+        className='pa3 ba b--green bg-lightest-blue'
         type="search" 
         placeholder='Search..' 
         onChange={searchChange}
