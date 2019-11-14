c15702f3a01405e2cc89d86ff9a6a6ef13067a0f
zxsteven <breathesteve@gmail.com>
2019-11-13 20:16:09 -0600

    circle ci

---
+++ server/server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import React from 'react'
+import fs from 'fs'
+import { renderToString } from 'react-dom/server'
+import App from './App'
+
+const app = express()
+
+app.use(express.static('/public'))
+
+const robots = JSON.parse(fs.readFileSync('/public/robots.json', 'utf8'))
+const RobofriendsApp = React.createElement(App)
+app.get('/', (request, response) => {
+  response.render('index', {
+    content: renderToString(RobofriendsApp({data: robots})
+    )
+  })
+})
